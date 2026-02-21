5fb0203f6d6a9e47f8983b53b3199a6324297009
SIRIDACH JAROENSIRI <siridech.jar24@gmail.com>
2026-02-21 21:10:15 +0700

    chang name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="MLOPS-PROJECT-1",
+    name="MLOPS-PROJECT-HOTEL-RESERVATION-PREDICTION",
     version="0.1",
     author="SIRIDACH JAROENSIRI",
     packages=find_packages(),
